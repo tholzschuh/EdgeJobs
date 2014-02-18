d468e54cc6c0f626302b8331c300833505e046f8
horoking <horoking@hotmail.de>
2014-02-18 19:32:07 +0100

    Finished JobCommand class

--- src/net/edgecraft/edgejobs/api/AbstractJobCommand.java
+++ src/net/edgecraft/edgejobs/api/AbstractJobCommand.java
@@ -3,6 +3,7 @@ package net.edgecraft.edgejobs.api;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import net.edgecraft.edgecore.EdgeCore;
 import net.edgecraft.edgecore.command.AbstractCommand;
 import net.edgecraft.edgecore.command.Level;
 import net.edgecraft.edgecore.user.User;
@@ -33,9 +34,33 @@ public abstract class AbstractJobCommand extends AbstractCommand {
 	@Override
 	public final boolean runImpl(Player player, User user, String[] args) throws Exception {
 		
+		boolean hasJob = false;
 		
+		if(isJob){
+			
+			AbstractJob j = JobManager.getUserJob(user);
+			
+			if(j.getName().equalsIgnoreCase(job))
+				hasJob = true;
+			
+		}
+		else{
+			
+			AbstractSidejob j = JobManager.getUserSidejob(user);
+			
+			if(j.getName().equalsIgnoreCase(job))
+				hasJob = true;
+			
+		}
 		
-		return false;
+		if(!hasJob){
+			
+			player.sendMessage(EdgeCore.errorColor + "Du darfst das nicht ohne den Job!");
+			
+			return true;
+		}
+		
+		return run(user, args);
 	}
 	
 	public abstract boolean run(User user, String[] args);
