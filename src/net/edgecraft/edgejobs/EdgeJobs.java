2dbcab47da7d5740f57e654eaa0cb0d1241387c6
horoking <horoking@hotmail.de>
2014-03-01 22:23:55 +0100

    Updated Mainclass
    Added static ChatColor and registered internal Command

--- src/net/edgecraft/edgejobs/EdgeJobs.java
+++ src/net/edgecraft/edgejobs/EdgeJobs.java
@@ -3,16 +3,21 @@ package net.edgecraft.edgejobs;
 import java.util.logging.Logger;
 
 import net.edgecraft.edgecore.EdgeCore;
+import net.edgecraft.edgecore.command.CommandHandler;
 import net.edgecraft.edgejobs.api.tasks.JobPayTask;
 import net.edgecraft.edgejobs.api.tasks.SidejobPayTask;
+import net.edgecraft.edgejobs.job.CommandJob;
 import net.edgecraft.edgejobs.util.ConfigHandler;
 import net.edgecraft.edgejobs.util.UtilListener;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class EdgeJobs extends JavaPlugin {
 
+	public static final ChatColor helpColor = ChatColor.GOLD;
+	
 	public static final String banner = "[EdgeJobs] ";
 	private static EdgeJobs instance;
 	private static final Logger log = EdgeCore.log;
@@ -30,6 +35,8 @@ public class EdgeJobs extends JavaPlugin {
 		ConfigHandler.prepare();
 		
 		getServer().getPluginManager().registerEvents(new UtilListener(), this);
+		
+		CommandHandler.getInstance().registerCommand(new CommandJob());
 		
 		startSchedulers();
 		
