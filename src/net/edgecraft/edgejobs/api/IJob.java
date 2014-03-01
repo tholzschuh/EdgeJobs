5369f6d586776153d13ff71140a80717a5d318d0
horoking <horoking@hotmail.de>
2014-03-02 00:11:06 +0100

    Changed IJob
    Added some Functions
    Added features for EdgeCuboid

--- src/net/edgecraft/edgejobs/api/IJob.java
+++ src/net/edgecraft/edgejobs/api/IJob.java
@@ -1,7 +1,10 @@
 package net.edgecraft.edgejobs.api;
 
 import net.edgecraft.edgecore.command.AbstractCommand;
+import net.edgecraft.edgecore.user.User;
+import net.edgecraft.edgecuboid.cuboid.types.CuboidType;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 /**
@@ -14,4 +17,7 @@ public interface IJob extends Listener {
 	public String getName();
 	public double getPay();
 	public AbstractCommand[] jobCommands();
+	public void printHelp(User u);
+	public void equipPlayer(Player p);
+	public CuboidType whereToStart();
 }
