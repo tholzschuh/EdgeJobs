5b9c5e7a21e9096d7cc376d9ab3da1d5b0f94e2c
horoking <horoking@hotmail.de>
2014-03-09 14:51:20 +0100

    Added Timber job.

---
+++ src/net/edgecraft/edgejobs/job/jobs/Timber.java
@@ -0,0 +1,37 @@
+package net.edgecraft.edgejobs.job.jobs;
+
+import org.bukkit.entity.Player;
+
+import net.edgecraft.edgecore.command.AbstractCommand;
+import net.edgecraft.edgecore.user.User;
+import net.edgecraft.edgecuboid.cuboid.types.CuboidType;
+import net.edgecraft.edgejobs.api.AbstractJob;
+import net.edgecraft.edgejobs.util.ConfigHandler;
+
+public class Timber extends AbstractJob {
+
+	public Timber() {
+		super("Holzfaeller", ConfigHandler.getJobPay("Holzfaeller"));
+	}
+
+	@Override
+	public AbstractCommand[] jobCommands() {
+		return new AbstractCommand[]{};
+	}
+
+	@Override
+	public void printHelp(User u) {
+		//TODO: Fill function
+	}
+
+	@Override
+	public void equipPlayer(Player p) {
+		//TODO: Fill function
+	}
+
+	@Override
+	public CuboidType whereToStart() {
+		return null;
+	}
+
+}
